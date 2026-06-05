7780029688f9f5185c2f0059766d76bb8b33108b
Paul Duffin <paulduffin@google.com>
2026-06-05 16:38:22 +0000

    Use android.annotation.Hide in HidePublicClasses
    Previously, it inserted `@hide`, this changes it to use
    `@android.annotation.Hide` instead. Will be used in a follow-up
    change to repackage the files.
    
    Flag: EXEMPT HOST_MODE
    Bug: 519195092
    Test: tools/srcgen/generate_android_icu4j.sh - uses @Hide
          m checkapi - no change
    Change-Id: If508a8579f6d088864f6708534e0c4a166dad538

--- tools/srcgen/currysrc/src/main/java/com/google/currysrc/processors/HidePublicClasses.java
+++ tools/srcgen/currysrc/src/main/java/com/google/currysrc/processors/HidePublicClasses.java
@@ -15,38 +15,73 @@
  */
 package com.google.currysrc.processors;
 
+import com.google.currysrc.api.process.Context;
 import com.google.currysrc.api.process.ast.TypeLocator;
-
-import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
-import org.eclipse.jdt.core.dom.Modifier;
-
+import com.google.currysrc.processors.AnnotationInfo.AnnotationClass;
+import java.util.Collections;
 import java.util.List;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.EnumDeclaration;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
 /**
  * Hides any public class that is not found in the allowlist.
  */
-public final class HidePublicClasses extends BaseJavadocTagClasses {
+public final class HidePublicClasses extends BaseAddAnnotation {
 
   private final List<TypeLocator> allowlist;
+  private final String reason;
 
   public HidePublicClasses(List<TypeLocator> allowlist, String reason) {
-    super("@hide " + reason);
     this.allowlist = allowlist;
+    this.reason = reason;
   }
 
-  @Override
-  protected boolean mustTag(AbstractTypeDeclaration node) {
-    boolean mustTag = false;
-    if ((node.getModifiers() & Modifier.PUBLIC) > 0) {
-      mustTag = true;
-      for (TypeLocator allowlistedType : allowlist) {
-        if (allowlistedType.matches(node)) {
-          mustTag = false;
-          break;
-        }
+  private static final AnnotationInfo HIDE =
+      new AnnotationInfo(
+          new AnnotationClass("android.annotation.Hide"),
+        Collections.emptyMap());
+
+  @Override public void process(Context context, CompilationUnit cu) {
+    ASTRewrite rewrite = context.rewrite();
+    cu.accept(new ASTVisitor() {
+      @Override
+      public boolean visit(AnnotationTypeDeclaration node) {
+        return visitAbstract(node);
       }
-    }
-    return mustTag;
+
+      @Override
+      public boolean visit(TypeDeclaration node) {
+        return visitAbstract(node);
+      }
+
+      @Override
+      public boolean visit(EnumDeclaration node) {
+        return visitAbstract(node);
+      }
+
+      private boolean visitAbstract(AbstractTypeDeclaration node) {
+        boolean mustHide = false;
+        if ((node.getModifiers() & Modifier.PUBLIC) > 0) {
+          mustHide = true;
+          for (TypeLocator allowlistedType : allowlist) {
+            if (allowlistedType.matches(node)) {
+              mustHide = false;
+              break;
+            }
+          }
+        }
+        if (mustHide) {
+          addAnnotationToBodyDeclaration(rewrite, node, HIDE, reason);
+        }
+        return true;
+      }
+    });
   }
 
   @Override
